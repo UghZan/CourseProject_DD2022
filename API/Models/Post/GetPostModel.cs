a17b79d1c0e575ee5104330f49210aa3d3c3cbaa
UghZan <yayminecrafters@gmail.com>
2022-12-18 20:56:08 +0300

    getpostmodel update
    - also returns guid

--- API/Models/Post/GetPostModel.cs
+++ API/Models/Post/GetPostModel.cs
@@ -6,6 +6,7 @@ namespace API.Models.Post
 {
     public class GetPostModel : GetPostContentModel
     {
+        public Guid Id { get; set; }
         public GetUserModelWithAvatar Author { get; set; } = null!;
         public DateTimeOffset CreationDate { get; set; }
         public int ReactionsCount { get; set; }
